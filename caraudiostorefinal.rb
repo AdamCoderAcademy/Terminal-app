929dfae803c222fffb60266de68ebad105df34e8
Adam Hyde <adamjameshyde88@hotmail.com>
2019-11-20 21:01:02 +1000

    Removed name variables

--- caraudiostorefinal.rb
+++ caraudiostorefinal.rb
@@ -11,7 +11,7 @@ package_D = "D, front speakers, tweeter seperates, rear speakers, speaker amplif
 # My welcome message and Main Menu options.
 
 until user_quits
-puts "\nWelcome to Car Stereo Store. \n
+puts "\nWelcome to Car Stereo Store \n
 If you would like to browse our store please select A.
 If you would like us to recommend a package please select B.
 If you would like to view your cart select C.
@@ -21,6 +21,7 @@ user_menu_selection = gets.chomp.upcase
 case user_menu_selection 
 
 when "A"
+
 puts "Here at Car Stereo Store we offer 4 packages sure to please anyone.
 This reduces cost on our side so we can pass the savings onto you, the customer.
 It also means that by bundling components together you can be sure they work perfectly for your needs \n
@@ -55,19 +56,18 @@ sleep 1
         user_menu_selection
 
     else user_package_selection
-        puts "Something went wrong, returning you to main menu"
+        puts "Something went wrong, returning you to main menu.".colorize(:red)
     end
     sleep 2
 
 
 when "B"
+
 puts "Welcome to the 'Car Stereo Store' guidance application".colorize(:red)
 sleep 2
-puts "Lets make it easy for you to choose a Car Audio System suitable for your music needs with only a few questions"
+puts "We make it easy for you to choose a Car Audio System suitable for your music needs with only a few questions"
 sleep 2
-puts "Lets start off with your Name ?".colorize(:red) + " "
-customer_name = gets.chomp 
-puts "Welcome #{customer_name} Lets get started ! \n"
+puts "Lets get started ! \n"
 sleep 1
         
 # First question for the user
@@ -77,7 +77,7 @@ puts "Are you happy to install a subwoofer ? Keeping in mind this adds additiona
 Please select from (1) yes or (2) no \n".colorize(:red)
 subwoofer = gets.chomp
 sleep 1
-puts "Thanks #{customer_name}, Second question"
+puts "Thanks, Second question"
 sleep 1
         
 # Second question for the user
@@ -85,7 +85,7 @@ puts "How important is sound quality to you ?
 Please select from (1) Not very, (2) Average and (3) Very important \n".colorize(:red)
 sound_q = gets.chomp
 sleep 1
-puts "Thanks #{customer_name}, Last question"
+puts "Thanks, Last question"
 sleep 1
     
 # Last question for the user
@@ -93,7 +93,7 @@ puts "What genre of music would you most prefer from the selection ?
 Please select from (1) Classical/Instrumental, (2) Rock/Metal, (3) Rap/Techno \n".colorize(:red)
 genre = gets.chomp
 sleep 1
-puts "Thanks #{customer_name}. See how easy that was ?"
+puts "Thanks, see how easy that was ?"
 sleep 1
 puts "Based on your selections"
 sleep 1
@@ -106,110 +106,111 @@ sleep 1.5
 #genre - 1 is "Classical/Instrumental", 2 is "Rock/Metal", 3 is "Rap/Techno"
         
     if subwoofer == "1" && sound_q == "1" && genre == "1"
-        puts "#{customer_name} we suggest package #{package_B}
+        puts "We suggest package #{package_B}
         \n
         Taking you back to the main menu"
     sleep 5
     elsif subwoofer == "1" && sound_q == "1" && genre == "2"
-        puts "#{customer_name} we suggest package #{package_A}
+        puts "We suggest package #{package_A}
         \n
         Taking you back to the main menu"
     sleep 5
     elsif subwoofer == "1" && sound_q == "1" && genre == "3"
-        puts "#{customer_name} we suggest package #{package_C}
+        puts "We suggest package #{package_C}
         \n
         Taking you back to the main menu"
     sleep 5
     elsif subwoofer == "1" && sound_q == "2" && genre == "1"
-        puts "#{customer_name} we suggest package #{package_C}
+        puts "We suggest package #{package_C}
         \n
         Taking you back to the main menu"
     sleep 5
     elsif subwoofer == "1" && sound_q == "2" && genre == "2"
-        puts "#{customer_name} we suggest package #{package_C}
+        puts "We suggest package #{package_C}
         \n
         Taking you back to the main menu"
     sleep 5
     elsif subwoofer == "1" && sound_q == "2" && genre == "3"
-        puts "#{customer_name} we suggest package #{package_C}
+        puts "We suggest package #{package_C}
         \n
         Taking you back to the main menu"
     sleep 5
     elsif subwoofer == "1" && sound_q == "3" && genre == "1"
-        puts "#{customer_name} we suggest package #{package_D}
+        puts "We suggest package #{package_D}
         \n
         Taking you back to the main menu"
     sleep 5
     elsif subwoofer == "1" && sound_q == "3" && genre == "2"
-        puts "#{customer_name} we suggest package #{package_D}
+        puts "We suggest package #{package_D}
         \n
         Taking you back to the main menu"
     sleep 5
     elsif subwoofer == "1" && sound_q == "3" && genre == "3"
-        puts "#{customer_name} we suggest package #{package_D}
+        puts "We suggest package #{package_D}
         \n
         Taking you back to the main menu"
     sleep 5
     elsif subwoofer == "2" && sound_q == "1" && genre == "1"
-        puts "#{customer_name} we suggest package #{package_A}
+        puts "We suggest package #{package_A}
         \n
         Taking you back to the main menu"
     sleep 5
     elsif subwoofer == "2" && sound_q == "1" && genre == "2"
-        puts "#{customer_name} we suggest package #{package_A}
+        puts "We suggest package #{package_A}
         \n
         Taking you back to the main menu" 
     sleep 5
     elsif subwoofer == "2" && sound_q == "1" && genre == "3"
-        puts "#{customer_name} we suggest package #{package_A}
+        puts "We suggest package #{package_A}
         \n
         Taking you back to the main menu"  
     sleep 5
     elsif subwoofer == "2" && sound_q == "2" && genre == "1"
-        puts "#{customer_name} we suggest package #{package_B}
+        puts "We suggest package #{package_B}
         \n
         Taking you back to the main menu"     
     sleep 5
     elsif subwoofer == "2" && sound_q == "2" && genre == "2"
-        puts "#{customer_name} we suggest package #{package_A}
+        puts "We suggest package #{package_A}
         \n
         Taking you back to the main menu"    
     sleep 5
     elsif subwoofer == "2" && sound_q == "2" && genre == "3"
-        puts "#{customer_name} we suggest package #{package_B}
+        puts "We suggest package #{package_B}
         \n
         Taking you back to the main menu"   
     sleep 5
     elsif subwoofer == "2" && sound_q == "3" && genre == "1"
-        puts "#{customer_name} we suggest package #{package_B}
+        puts "We suggest package #{package_B}
         \n
         Taking you back to the main menu"
     sleep 5
     elsif subwoofer == "2" && sound_q == "3" && genre == "2"
-        puts "#{customer_name} we suggest package #{package_B}
+        puts "We suggest package #{package_B}
         \n
         Taking you back to the main menu"   
     sleep 5
     elsif subwoofer == "2" && sound_q == "3" && genre == "3"
-        puts "#{customer_name} we suggest package #{package_B}
+        puts "We suggest package #{package_B}
         \n
         Taking you back to the main menu"
     sleep 5
 
     else 
-        puts "Sorry you have input information incorrectly #{customer_name}, now taking you back to main menu"
+        puts "Sorry something went wrong, now taking you back to main menu".colorize(:red)
     sleep 2
     end
 
 
 when "C"
+
 sleep 1
     if cart = cart
         puts "Currently inside your cart is..
         Package #{cart}"   
     else cart =
         puts "Your cart is still empty. \n 
-        Taking you back to the main menu"
+        Taking you back to the main menu".colorize(:red)
     end
 sleep 3
 
@@ -220,7 +221,7 @@ user_quits = true
 
 else
 sleep 1
-puts "Not a valid selection returning you to the main menu please try again"
+puts "Not a valid selection returning you to the main menu please try again".colorize(:red)
 sleep 3
 puts "\n"
 end
